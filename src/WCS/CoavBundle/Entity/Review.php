4e9fd7ebb4dc220658813f821231a02a05ec4349
Unknown <stevendsb@gmail.com>
2017-11-12 17:28:16 +0100

    ajout table review

---
+++ src/WCS/CoavBundle/Entity/Review.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace WCS\CoavBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Review
+ *
+ * @ORM\Table(name="review")
+ * @ORM\Entity(repositoryClass="WCS\CoavBundle\Repository\ReviewRepository")
+ */
+class Review
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text")
+     */
+    private $text;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="userRated", type="integer")
+     */
+    private $userRated;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="reviewAuthor", type="integer")
+     */
+    private $reviewAuthor;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="publicationDate", type="datetime")
+     */
+    private $publicationDate;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="note", type="smallint")
+     */
+    private $note;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Review
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set userRated
+     *
+     * @param integer $userRated
+     *
+     * @return Review
+     */
+    public function setUserRated($userRated)
+    {
+        $this->userRated = $userRated;
+
+        return $this;
+    }
+
+    /**
+     * Get userRated
+     *
+     * @return int
+     */
+    public function getUserRated()
+    {
+        return $this->userRated;
+    }
+
+    /**
+     * Set reviewAuthor
+     *
+     * @param integer $reviewAuthor
+     *
+     * @return Review
+     */
+    public function setReviewAuthor($reviewAuthor)
+    {
+        $this->reviewAuthor = $reviewAuthor;
+
+        return $this;
+    }
+
+    /**
+     * Get reviewAuthor
+     *
+     * @return int
+     */
+    public function getReviewAuthor()
+    {
+        return $this->reviewAuthor;
+    }
+
+    /**
+     * Set publicationDate
+     *
+     * @param \DateTime $publicationDate
+     *
+     * @return Review
+     */
+    public function setPublicationDate($publicationDate)
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publicationDate
+     *
+     * @return \DateTime
+     */
+    public function getPublicationDate()
+    {
+        return $this->publicationDate;
+    }
+
+    /**
+     * Set note
+     *
+     * @param integer $note
+     *
+     * @return Review
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return int
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+}
+
